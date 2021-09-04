bc4bac47d9ee49a38282ac111c94fbb6b5727906
Camelia Brennan <cameliabrennan@gmail.com>
2021-09-04 12:17:41 -0500

    added gitignore for mp3 files

--- convert.py
+++ convert.py
@@ -1,7 +1,7 @@
 from mutagen.mp3 import MP3
 import os
 
-mp3_dir = os.listdir("/Volumes/Untitled/python_mutagen/mp3s")
+mp3_dir = os.listdir("/Volumes/Untitled/python_mutagen/mp3_meta_data/mp3s")
 print (mp3_dir)
 # function to convert the seconds into readable format
 def convert(seconds):
@@ -15,14 +15,14 @@ def convert(seconds):
 
 for song in mp3_dir:
     if song.endswith(".mp3"):
-        audio = MP3("/Volumes/Untitled/python_mutagen/mp3s/" + song)
+        audio = MP3("/Volumes/Untitled/python_mutagen/mp3_meta_data/mp3s/" + song)
         audio_info = audio.info    
         length_in_secs = int(audio_info.length)
         hours, mins, seconds = convert(length_in_secs)
         print(song)
-        print("Hours:", hours)
-        print("Minutes:", mins)
-        print("Seconds:", seconds)
+        #print("Hours:", hours)
+        #print("Minutes:", mins)
+        #print("Seconds:", seconds)
         print("Total Seconds", length_in_secs )
 
 #TODO:
